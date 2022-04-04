830890d15055a6b8bcff6193982a3ee965662dd7
steven ding <76521528+yaoyangdi@users.noreply.github.com>
2022-04-04 23:56:24 +1000

    1) Fields of User entity added 2) Modification on url name "/getByEmail" instead of "getbyEmail" 3) API url with endpoint "/updatepassword/<email>" to update users' password by their email 4) Add unique key constraint on user_email to avoid duplication

--- routes/user.py
+++ routes/user.py
@@ -16,50 +16,44 @@ def getUsers():
     users = User.query.all()
     return jsonify(users)
 
-@user.route('/getbyEmail/<email>', methods=['GET'])
+
+@user.route('/getByEmail/<email>', methods=['GET'])
 def getUserByEmail(email):
     # params = [i for i in request.args.keys()]
-    user = User.query.filter_by(user_email=email)
-    return jsonify(user.first())
+    user = User.query.filter_by(user_email=email).first()
+    if user:
+        return jsonify(user)
+    else:
+        return jsonify({"message": "user not found"})
+
 
 @user.route('/add', methods=['POST'])
 def addUser():
     # if bcrypt.hashpw(password, user['password'].encode('utf-8')) == user['password'].encode('utf-8'):
     data = request.form
-    userName = data['username']
+    username = data['username']
     lname = data['lastname']
     fname = data['firstname']
     email = data['email']
     password = data['password']
 
-
-    user = User(user_username=userName, user_firstname=fname, user_lastname=lname, user_email=email, user_password=password, user_contributionscore=0)
+    user = User(user_username=username, user_firstname=fname, user_lastname=lname, user_email=email, user_password=password, user_contributionscore=0)
     db.session.add(user)
     db.session.commit()
     return redirect(url_for('api.users.getUsers'))
 
 
-@user.route('/update/<id>', methods=['PUT', 'GET'])
-def update(id):
-    user = User.query.get(id)
-
-    lName = request.args.get('lname', None)
-    fName = request.args.get('fname', None)
-    email = request.args.get('email', None)
-    password = request.args.get('password', None)
-
-    if lName != None:
-        user.user_lastname = lName
-    if fName != None:
-        user.user_firstname = fName
-    if email != None:
-        user.user_email = email
-    if password != None:
+@user.route('/updatepassword/<email>', methods=['PUT'])
+def update(email):
+    user = User.query.filter_by(user_email=email).first()
+    if user:
+        data = request.form
+        password = data['password']
         user.user_password = password
-
-    db.session.commit()
-
-    return jsonify(user)
+        db.session.commit()
+        return jsonify(user)
+    else:
+        return jsonify({"message": "user not found"})
 
 
 @user.route('/delete/<id>', methods=['DELETE', 'GET'])
@@ -79,3 +73,4 @@ def deleteAll():
     db.session.query(User).delete()
     db.session.commit()
     return redirect(url_for('api.users.getUsers'))
+
