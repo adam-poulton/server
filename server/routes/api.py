b41cb64f953ef298a53e1e263ee16c2fecf0f202
adamp <adam.poulton5@gmail.com>
2022-04-06 17:41:40 +1000

    fix typo

--- server/routes/api.py
+++ server/routes/api.py
@@ -2,9 +2,9 @@ from flask import jsonify, Blueprint
 
 from server.routes.products import product
 from server.routes.user import user
-from server.routes.starredProducts import starredProduct
+from server.routes.favourite import favourites
 
 api = Blueprint('api', __name__)
 api.register_blueprint(user, url_prefix='/user')
 api.register_blueprint(product, url_prefix='/product')
-# api.register_blueprint(starredProduct, url_prefix='/star')
+api.register_blueprint(favourites, url_prefix='/favourite')
