cdb7f4326be680ab01cba336fe739b9f8e6a8123
adamp <adam.poulton5@gmail.com>
2022-04-06 17:47:25 +1000

    remove favourite route

--- server/routes/api.py
+++ server/routes/api.py
@@ -2,9 +2,8 @@ from flask import jsonify, Blueprint
 
 from server.routes.products import product
 from server.routes.user import user
-from server.routes.favourite import favourites
+
 
 api = Blueprint('api', __name__)
 api.register_blueprint(user, url_prefix='/user')
 api.register_blueprint(product, url_prefix='/product')
-api.register_blueprint(favourites, url_prefix='/favourite')
