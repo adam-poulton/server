43841689cffd1df7c2ecc45e117cc1b1cf0eb37c
adamp <adam.poulton5@gmail.com>
2022-04-06 17:39:39 +1000

    fix typo

--- server/routes/favourite.py
+++ server/routes/favourite.py
@@ -6,31 +6,31 @@ from pathlib import Path
 from flask import Blueprint, render_template, url_for, request, jsonify
 from werkzeug.utils import redirect, secure_filename
 
-from ..models import Favourite, db, Product
+from ..models import Favourite, db
 
 
-starredProduct = Blueprint('favourites', __name__)
+favourites = Blueprint('favourites', __name__)
 
 
-@starredProduct.route('/display')
+@favourites.route('/display')
 def display_products():
     return render_template('favourites.html', Favourites=Favourite.query.all())
 
 
-@starredProduct.route('/get', methods='GET')
+@favourites.route('/get', methods='GET')
 def get_favourite():
     favourite = Favourite.query.all()
     return jsonify(favourite)
 
 
-@starredProduct.route('/add/<product_id>', methods='POST')
+@favourites.route('/add/<product_id>', methods='POST')
 def add_favourite(product_id):
     favourite = Favourite(product_id = product_id)
     db.session.add(favourite)
     return redirect(url_for('api.starProducts.get_favourite', favourite_id=favourite.favourite_id))
 
 
-@starredProduct.route('/productDetails')
+@favourites.route('/productDetails')
 def get_products():
     starredProducts = Favourite.query.all()
 
