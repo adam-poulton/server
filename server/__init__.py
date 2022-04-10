37030ad9525868d5251b47a9afec90c8c4a239cf
adamp <adam.poulton5@gmail.com>
2022-04-11 02:23:16 +1000

    created entry function

--- server/__init__.py
+++ server/__init__.py
@@ -1,4 +1,25 @@
-from server import application
+from flask import Flask, render_template, Blueprint
+from server.database import db_session, init_db
 
 
+def main():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+    api = Blueprint('api', __name__)
+    app.register_blueprint(api, url_prefix='/api')
+
+    @app.route('/')
+    def index():
+        return render_template('index.html')
+
+    @app.teardown_appcontext
+    def shutdown_session(exception=None):
+        db_session.remove()
+
+    init_db()
+    app.run()
+
+
+if __name__ == "__main__":
+    main()
