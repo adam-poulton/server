bcda4f81d219c16381d84dbb05ddd6b770847dba
adamp <adam.poulton5@gmail.com>
2022-04-11 17:41:15 +1000

    disabled favourites route to bugfix

--- server/routes/api.py
+++ server/routes/api.py
@@ -1,9 +1,9 @@
 from flask import Blueprint
 from server.routes.product import product
 from server.routes.user import user
-from server.routes.favourite import favourite
+# from server.routes.favourite import favourite
 
 api = Blueprint('api', __name__)
 api.register_blueprint(product, url_prefix='/product')
 api.register_blueprint(user, url_prefix='/user')
-api.register_blueprint(favourite, url_prefix='/favourite')
+# api.register_blueprint(favourite, url_prefix='/favourite')
