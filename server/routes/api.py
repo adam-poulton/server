bd6d4b26039628452f29239dddbbd4847f3842e2
Yaoyang Ding <dingyaoyang2019@gmail.com>
2022-04-14 04:11:42 +1000

    Add new model named Feedback and add the relevant APIs for creating and getting user feedback.

--- server/routes/api.py
+++ server/routes/api.py
@@ -1,9 +1,11 @@
 from flask import Blueprint
 from server.routes.product import product
 from server.routes.user import user
+from server.routes.feedback import feedback
 # from server.routes.favourite import favourite
 
 api = Blueprint('api', __name__)
 api.register_blueprint(product, url_prefix='/product')
 api.register_blueprint(user, url_prefix='/user')
 # api.register_blueprint(favourite, url_prefix='/favourite')
+api.register_blueprint(feedback, url_prefix='/feedback')
