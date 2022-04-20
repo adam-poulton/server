3af066aac8fd437875843732c51ed796befcbe8a
Yaoyang Ding <dingyaoyang2019@gmail.com>
2022-04-20 19:41:46 +1000

    Root url for review API added.

--- server/routes/api.py
+++ server/routes/api.py
@@ -4,6 +4,7 @@ from server.routes.user import user
 from server.routes.feedback import feedback
 from server.routes.favourite import favourite
 from server.routes.scan import scan
+from server.routes.review import review
 
 api = Blueprint('api', __name__)
 api.register_blueprint(product, url_prefix='/product')
@@ -11,3 +12,4 @@ api.register_blueprint(user, url_prefix='/user')
 api.register_blueprint(favourite, url_prefix='/favourite')
 api.register_blueprint(feedback, url_prefix='/feedback')
 api.register_blueprint(scan, url_prefix='/scan')
+api.register_blueprint(review, url_prefix='/review')
