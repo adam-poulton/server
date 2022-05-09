f2b65df499aba129db6bad9788d77f5483d0d31d
adamp <adam.poulton5@gmail.com>
2022-05-10 02:30:19 +1000

    modified form

--- server/routes/admin.py
+++ server/routes/admin.py
@@ -1,25 +1,31 @@
 from flask import Blueprint, jsonify, request, render_template
 from flask_wtf import FlaskForm
-from flask_wtf.file import FileField, FileRequired
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+from flask_uploads import UploadSet, IMAGES
+from wtforms import SubmitField
 
 from server.services import nutrition_detector as detect
 
 admin = Blueprint('admin', __name__)
 
+images = UploadSet('images', IMAGES)
+
 
 class PhotoForm(FlaskForm):
-    image = FileField(validators=[FileRequired()])
+    image = FileField(validators=[FileAllowed(images, 'Images only!'), FileRequired('Select an image')])
+    submit = SubmitField('Scan Image')
 
 
 @admin.route('/upload', methods=['GET', 'POST'])
 def upload():
     form = PhotoForm()
+    result = None
 
     if form.validate_on_submit():
         img = form.image.data
-        return jsonify(detect.from_raw(img))
+        result = detect.from_raw(img)
 
-    return render_template('upload.html', form=form)
+    return render_template('upload.html', form=form, result=result)
 
 
 def process():
