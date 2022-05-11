549975ca91b00ef57f68081126582177b093b50f
adamp <adam.poulton5@gmail.com>
2022-05-11 15:50:48 +1000

    small change

--- ocr_example.py
+++ ocr_example.py
@@ -5,9 +5,9 @@ from ocr.detect import NutritionDetectionPipeline
 
 if __name__ == '__main__':
     detect_nutrition = NutritionDetectionPipeline()
-    image_url = 'https://res.cloudinary.com/hlvl5cgpe/image/upload/v1652161903/twfonmhsw9rnfhyvdjvf.jpg'
-    result = detect_nutrition.from_url(image_url, debug=True)
-    image_path = os.path.join('ocr/data/images', f'label-{1}.jpg')
-    # result = detect_nutrition.from_path(image_path, debug=True)
-    print(json.dumps(result))
-    # print(''.join([f'{k:18} {v["value"]:8} {v["unit"]:8}\n' for k, v in result.items()]))
+    # image_url = 'https://res.cloudinary.com/hlvl5cgpe/image/upload/v1652161903/twfonmhsw9rnfhyvdjvf.jpg'
+    # result = detect_nutrition.from_url(image_url, debug=True)
+    image_path = os.path.join('ocr/data/images', f'label-{5}.jpg')
+    result = detect_nutrition.from_path(image_path, debug=True)
+    # print(json.dumps(result))
+    print(''.join([f'{k:18} {v["value"]:8} {v["unit"]:4}\n' for k, v in result.items()]))
